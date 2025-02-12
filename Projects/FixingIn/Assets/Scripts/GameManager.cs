4a87b69096c642c932bbb88398adf8c747d0c06b
LucasMeldrum <150073579+LucasMeldrum@users.noreply.github.com>
2025-02-12 11:06:26 -0500

    feat: added animation and scene changer for BrickBreaker game

--- Projects/FixingIn/Assets/Scripts/GameManager.cs
+++ Projects/FixingIn/Assets/Scripts/GameManager.cs
@@ -3,36 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
     public int lives;
     public int points;
 
-    public Image heartImage;
-
-    public static GameManager S;
-    
     // Start is called before the first frame update
-    void Awake() {
-        S = this;
-    }
-
-    void Start() {
+    void Start()
+    {
         lives = 4;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
-    public void LoseLife() {
-        lives -= 1;
-        // full life = 1 // 3
-        heartImage.fillAmount = (lives * .2f);
-    }
-    
-    public void AddPoint(int numPoints) {
-        points += numPoints;
     }
 }
